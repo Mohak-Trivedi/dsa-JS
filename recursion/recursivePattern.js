afc1f794290213da8f67536923b8f7e8f15f3f1d
Mohak-Trivedi <trivedimohak@gmail.com>
2024-07-16 23:51:14 +0530

    recursive Pattern follow up

--- recursion/recursivePattern.js
+++ recursion/recursivePattern.js
@@ -1,14 +1,25 @@
 // Problem: Given n, print n rows, such that ith row has i number of stars
-function f(n, row) {
+// function f(n, row) {
+//     if(row > n) return;
+
+//     let rowPattern = "";
+//     for(let i = 1;i <= row;i++) {
+//         rowPattern += "*";
+//     }
+//     console.log(rowPattern);
+
+//     f(n, row + 1);
+// }
+
+// f(5, 1);
+
+// Follow up: Solve without using not even a single for-loop.
+function f(n, row, rowPattern) {
+    // Base Case
     if(row > n) return;
 
-    let str = "";
-    for(let i = 1;i <= row;i++) {
-        str += "*";
-    }
-    console.log(str);
-
-    f(n, row + 1);
+    console.log(rowPattern);
+    f(n, row+1, rowPattern + "*");
 }
 
-f(5, 1);
+f(5, 1, "*");
