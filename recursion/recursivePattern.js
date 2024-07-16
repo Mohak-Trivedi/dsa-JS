1c717e9dbc87fe88ceec9465567359df37136a5c
Mohak-Trivedi <trivedimohak@gmail.com>
2024-07-17 00:21:27 +0530

    recursivePattern.js follow up 2

--- recursion/recursivePattern.js
+++ recursion/recursivePattern.js
@@ -14,12 +14,33 @@
 // f(5, 1);
 
 // Follow up: Solve without using not even a single for-loop.
-function f(n, row, rowPattern) {
+// function f(n, row, rowPattern) {
+//     // Base Case
+//     if(row > n) return;
+
+//     console.log(rowPattern);
+//     f(n, row+1, rowPattern + "*");
+// }
+
+// f(5, 1, "*");
+
+// Follow up: The above solution is costly space-wise because strings are immutable
+// in JS leading to formation of new string each time the function is called.
+function f(n, row, col) {
     // Base Case
     if(row > n) return;
 
-    console.log(rowPattern);
-    f(n, row+1, rowPattern + "*");
+    // Current row over
+    if(col > row) {
+        console.log(""); // Print a newline character at the end of the row
+        f(n, row + 1, 1); // start with next row
+        return;
+    }
+
+    // self work
+    process.stdout.write("*"); // print current row current column's *
+    f(n, row, col + 1); // next column in this row
+    return;
 }
 
-f(5, 1, "*");
+f(5, 1, 1);
