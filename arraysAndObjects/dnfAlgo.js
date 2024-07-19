fd541002e06c224c88af80fe75913d79b6a75b23
Mohak-Trivedi <trivedimohak@gmail.com>
2024-07-20 00:18:01 +0530

    dnf alt approach and explanation

--- arraysAndObjects/dnfAlgo.js
+++ arraysAndObjects/dnfAlgo.js
@@ -15,6 +15,15 @@
 // But, we are unable to do it in a single pass here, so can't use this approach.
 
 // Approach 2:
+// arr[0, i-1] i.e. on left of ith element: all 0s
+// arr[j+1, n-1] i.e. on right of jth element: all 1s
+// arr[i, j] i.e. from i to j: unsorted i.e. mixture of 0s and 1s
+// Now for each element in arr[i, j], we need to put it before i (if 0) or 
+// after j (if 1).
+// So, if(arr[i] === 0), simply move i to its right so it autmatically falls in 
+// arr[0, i-1], else i.e. arr[i]===1, then swap it with arr[j] and now since we
+// are sure that arr[j]===1, move j to left i.e. j-- so that this 1 automatically
+// falls in arr[i+1, n-1]
 function swap(arr, i, j) {
     let temp = arr[i];
     arr[i] = arr[j];
@@ -38,3 +47,30 @@ function separate(arr) {
 let arr = [1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1];
 separate(arr);
 console.log(arr);
+
+// Doubts on Approach 2:
+// Why not do i++ and why just j-- after swapping arr[i] with arr[j] when arr[i]
+// was found as 1?
+// Answer:
+// Because, we are sure that the value that moved from i to j was 1, but we are 
+// not sure whether the value that moved from j to i was 0 or not, if we would've
+// been sure that it will always be 0 then we would've also moved i++, but that's 
+// not always the case.
+
+// Why check only for i, why not also check if(arr[j]===1) j-- ?
+// Answer:
+// Well, we can do that as well. However, it is not really required and our algo
+// works totally fine even without that additional condition.
+
+// Note:
+// We can also, make it j-based:
+/*
+while(i < j) {
+    if(arr[j] === 1) {
+        j--;
+    } else {
+        swap(arr, i, j);
+        i++;
+    }
+}
+*/
