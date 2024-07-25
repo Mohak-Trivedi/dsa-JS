c4d49152c70081d1e95d6e697e709126b525ed41
Mohak-Trivedi <trivedimohak@gmail.com>
2024-07-25 22:08:16 +0530

    solved reverse an array

---
+++ arrayProblems/revArr.js
@@ -0,0 +1,20 @@
+// Problem: Given an array, reverse it without creating a new array.
+
+function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+function revArr(arr) {
+    let i = 0, j = arr.length - 1;
+    while(i < j) {
+        swap(arr, i, j);
+        i++;
+        j--;
+    }
+}
+
+let arr = [2, 4, 8, 6, 5];
+revArr(arr);
+console.log(arr);
