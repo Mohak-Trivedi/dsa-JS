bc28a0b809181f26be587a7994ac38793599118e
Mohak-Trivedi <trivedimohak@gmail.com>
2024-08-03 00:08:26 +0530

    square matrix transpose optimized

--- array2d/transpose.js
+++ array2d/transpose.js
@@ -6,7 +6,6 @@
 // Create an empty col X row result matrix, where given matrix is of row X col dimension.
 // Populate this by iterating over each element of given matrix using:
 // result[col][row] = matrix[row][col]
-
 /**
  * @param {number[][]} matrix
  * @return {number[][]}
@@ -25,3 +24,23 @@ var transpose = function(matrix) {
 
     return result;
 };
+
+// If we could've got only square matrices as inputs:
+// TC: O((m * n) / 2), SC: O(1)
+// As the input matrix and the transpose would be of the same dimensions, the 
+// indices would be deterministic, so we know for sure that we can do
+// in-place swapping.
+// NOTE: No need to iterate over elements before the main diagonal as it will
+// lead to re-swapping and we will get back the original matrix instead of the 
+// transpose.
+// var transpose = function(matrix) {
+//     for(let row = 0;row < matrix.length;row++) {
+//         for(let col = row + 1;col < matrix[0].length;col++) {
+//             let temp = matrix[row][col];
+//             matrix[row][col] = matrix[col][row];
+//             matrix[col][row] = temp;
+//         }
+//     }
+
+//     return matrix;
+// }
