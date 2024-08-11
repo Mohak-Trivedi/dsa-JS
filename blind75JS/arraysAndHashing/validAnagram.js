e03481e97254e10505e040f73ddb21b150b170a1
Mohak-Trivedi <trivedimohak@gmail.com>
2024-08-11 12:44:32 +0530

    valid anagram hashmap approach

---
+++ blind75JS/arraysAndHashing/validAnagram.js
@@ -0,0 +1,53 @@
+// Problem: https://leetcode.com/problems/valid-anagram/
+
+// Approach 1: TC: O(s + t), SC: O(s)
+// Using HashMap
+// Create frequency map for string s.
+// For each char of string t:
+// - If it doesn't exist in freqMap, continue to next char.
+// - If it exists, then decrement its frequency.
+// For each key-value pair of freqMap:
+// - If value is NOT 0, return false.
+// - return true
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  const freqMap = new Map();
+
+  addFreq(freqMap, s);
+
+  subtractFreq(freqMap, t);
+
+  return checkFreq(freqMap);
+};
+
+const addFreq = (map, str) => {
+  for (const char of str) {
+    const freq = (map.get(char) || 0) + 1;
+
+    map.set(char, freq);
+  }
+};
+
+const subtractFreq = (map, str) => {
+  for (const char of str) {
+    if (!map.has(char)) continue;
+
+    const freq = map.get(char) - 1;
+    map.set(char, freq);
+  }
+};
+
+const checkFreq = (map) => {
+  for (const [char, freq] of map) {
+    if (freq !== 0) return false;
+  }
+
+  return true;
+};
