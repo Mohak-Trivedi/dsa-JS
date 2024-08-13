0752e79e014a5cf3a31e857e03474c372c58801c
Mohak-Trivedi <trivedimohak@gmail.com>
2024-08-13 23:32:30 +0530

    3 sum

---
+++ blind75JS/twoPointers/threeSum.js
@@ -0,0 +1,45 @@
+// Problem: https://leetcode.com/problems/3sum/
+
+// Optimized Approach: TC: O(N^2), SC: O(1)
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  nums.sort((a, b) => a - b); // sort to be able to use 2-pointer approach
+
+  let res = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+
+    if (num > 0) break; // not possible to form triplet of sum 0 for this and further elements as first number as the sum will always be >0
+
+    if (i > 0 && num === nums[i - 1]) continue; // avoid duplicate triplets. Same first number -> Same triplet
+
+    let l = i + 1,
+      r = nums.length - 1;
+    while (l < r) {
+      const threeSum = num + nums[l] + nums[r];
+
+      if (threeSum < 0) {
+        l++;
+      } else if (threeSum > 0) {
+        r--;
+      } else {
+        // found triplet
+        res.push([num, nums[l], nums[r]]);
+
+        l++;
+        r--;
+
+        while (l < r && nums[l] === nums[l - 1]) {
+          // avoid duplicate triplets. Same first and second number -> same 3rd number -> same triplet
+          l++;
+        }
+      }
+    }
+  }
+
+  return res;
+};
