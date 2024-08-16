41804b97fde75f2a0266fdca1a9a1045fb0c49bb
Mohak-Trivedi <trivedimohak@gmail.com>
2024-08-16 22:39:49 +0530

    group anagrams optimized approach

--- blind75JS/arraysAndHashing/groupAnagrams.js
+++ blind75JS/arraysAndHashing/groupAnagrams.js
@@ -1,6 +1,6 @@
 // Problem: https://leetcode.com/problems/group-anagrams/
 
-// Brute Force Approach: ime O(N * (K * log(K))) | Space O(N * K)
+// Brute Force Approach: TC: O(N * (K * log(K))), SC: O(N * K), where N: number of strings, K: average number of characters in each string
 // Using sorting and hashmap.
 // Create Hashmap: Sorted word: array of anagrams of this sorted word
 // Return array of all values of this hashmap
@@ -32,3 +32,42 @@ var reorder = function (str) {
     .sort((a, b) => a.localeCompare(b))
     .join("");
 };
+
+// Optimized Approach: TC: O(N * K), SC: O(N * K)
+// Using HashMap and frequency array.
+// Create HashMap: Stringified Frequency Array: Array of strings having this frequency array
+// Return array containing all the values of the hashmap.
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  const map = new Map();
+
+  groupWords(map, strs);
+
+  return [...map.values()];
+};
+
+var groupWords = function (map, words) {
+  for (const original of words) {
+    const hash = getHash(original);
+    const values = map.get(hash) || [];
+
+    values.push(original);
+    map.set(hash, values);
+  }
+};
+
+var getHash = function (str) {
+  const freqArr = new Array(26).fill(0);
+
+  for (const char of str) {
+    const idx = getCode(char);
+    freqArr[idx]++;
+  }
+
+  return freqArr.toString();
+};
+
+var getCode = (c) => c.charCodeAt(0) - "a".charCodeAt(0);
