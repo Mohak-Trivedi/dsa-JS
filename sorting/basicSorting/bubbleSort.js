d4a133fbfe29956a76b90868b3843bbca3caee4e
Mohak-Trivedi <trivedimohak@gmail.com>
2024-08-22 00:22:17 +0530

    bubble sort explanation

--- sorting/basicSorting/bubbleSort.js
+++ sorting/basicSorting/bubbleSort.js
@@ -1,10 +1,18 @@
 // Implement Bubble Sort algo.
 
+// Based on real-life bubbling. Large bubbles get bubbled up first and then the
+// smallest one at the end.
+// In Bubble Sort, we consider 2 subarrays within the array, unsorted array
+// (initially entire array) on left and sorted subarray (initially empty) on right.
+// In each iteration, the largest element from the unsorted subarray gets
+// bubbled to the start of the sorted subarray.
+
 // TC: O(N^2) for worst and average case, Omega(N) for best case, SC: O(1)
 // Best case: Array is already started.
 
 // Comparison-based, In-place, Stable sorting algorithm.
 // It is swap-heavy.
+// N^2 comparisons and N^2 swaps in worst case.
 
 function bubbleSort(arr) {
   let n = arr.length;
