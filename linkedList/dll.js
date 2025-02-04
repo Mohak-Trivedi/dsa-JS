e504206079ce1194855e1ec350c6f0f88dc2fe2c
Mohak-Trivedi <trivedimohak@gmail.com>
2025-02-04 22:50:14 +0530

    doubly linked list - remove head

--- linkedList/dll.js
+++ linkedList/dll.js
@@ -19,6 +19,18 @@ function addAtHead(value, head) {
   return head;
 }
 
+function removeAtHead(head) {
+  if (head === null) return null;
+
+  if (head.next === null) return null;
+
+  const nextNode = head.next;
+  head.next = null;
+  nextNode.prev = null;
+  head = nextNode;
+  return head;
+}
+
 function display(head) {
   if (head === null) return;
 
@@ -40,4 +52,6 @@ head = addAtHead(10, head);
 head = addAtHead(20, head);
 head = addAtHead(30, head);
 head = addAtHead(40, head);
+display(head);
+head = removeAtHead(head);
 display(head);
